23c644af6fff6afb2c5bd50048c9f562f5b15629
ucandoit1010 <ucandoit1010@gmail.com>
2017-12-18 14:36:02 +0800

    Add New Class

---
+++ Models/NumberCalculation/CompositeNumber.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using MYGOD539.Models.DataOperation;
+
+namespace MYGOD539.Models.NumberCalculation
+{
+    public class CompositeNumber : Calculator
+    {
+        protected CompositeNumber()
+        {
+        }
+
+        public override DataTable CalcMethod(int year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
